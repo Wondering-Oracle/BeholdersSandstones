c31d1e4ecae36bd1f5720be07256e6130096edac
Wondering-Oracle <73407726+Wondering-Oracle@users.noreply.github.com>
2021-11-01 18:05:47 -0400

    Sandstone Walls, Crafting Recipes for Smooth Black Sandstone

--- src/main/java/com/silicateseer/beholderssandstones/BeholdersSandstones.java
+++ src/main/java/com/silicateseer/beholderssandstones/BeholdersSandstones.java
@@ -3,6 +3,7 @@ package com.silicateseer.beholderssandstones;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 import net.minecraft.block.SandBlock;
@@ -21,11 +22,22 @@ import org.apache.logging.log4j.Logger;
 
 public class BeholdersSandstones implements ModInitializer {
 	public static final String MODID = "beholderssandstones";
+	
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	
+	public static final Block CUT_SANDSTONE_WALL = register("cut_sandstone_wall",
+			new WallBlock(FabricBlockSettings.copyOf(Blocks.CUT_SANDSTONE)), ItemGroup.DECORATIONS);
+	public static final Block SMOOTH_SANDSTONE_WALL = register("smooth_sandstone_wall",
+			new WallBlock(FabricBlockSettings.copyOf(Blocks.SMOOTH_SANDSTONE)), ItemGroup.DECORATIONS);
+	
+	public static final Block CUT_RED_SANDSTONE_WALL = register("cut_red_sandstone_wall",
+			new WallBlock(FabricBlockSettings.copyOf(Blocks.CUT_RED_SANDSTONE)), ItemGroup.DECORATIONS);
+	public static final Block SMOOTH_RED_SANDSTONE_WALL = register("smooth_red_sandstone_wall",
+			new WallBlock(FabricBlockSettings.copyOf(Blocks.SMOOTH_RED_SANDSTONE)), ItemGroup.DECORATIONS);
+
 	public static final Block BLACK_SAND = register("black_sand",
 			new SandBlock(14866662, FabricBlockSettings.of(Material.AGGREGATE, MapColor.IRON_GRAY).strength(0.5F).sounds(BlockSoundGroup.SAND)), ItemGroup.BUILDING_BLOCKS);
 	public static final Block BLACK_SANDSTONE = register("black_sandstone",
@@ -53,6 +65,8 @@ public class BeholdersSandstones implements ModInitializer {
 	public static final Block SMOOTH_BLACK_SANDSTONE_WALL = register("smooth_black_sandstone_wall",
 			new WallBlock(FabricBlockSettings.copyOf(SMOOTH_BLACK_SANDSTONE)), ItemGroup.DECORATIONS);
 	
+	public static final Block PRISMARINE_SAND = register("prismarine_sand",
+			new SandBlock(6200996, FabricBlockSettings.of(Material.AGGREGATE, MapColor.CYAN).strength(0.5F).sounds(BlockSoundGroup.SAND)), ItemGroup.BUILDING_BLOCKS);
 
 	@Override
 	public void onInitialize() {
